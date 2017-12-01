687b59c05ecf6ae83ec750b41f3239b7987d2644
Michael Andrés Murcia Contreras <mamurciac@unal.edu.co>
2017-12-01 10:07:34 -0500

    Identando archivos de código

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,65 +1,64 @@
 /*!
  * Bootstrap v3.2.0 (http://getbootstrap.com)
- * Copyright 2011-2014 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * Derechos de Autor 2011-2014 Twitter, Inc.
+ * Licencia bajo MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
-if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript requires jQuery') }
+if(typeof jQuery === 'undefined'){
+  throw new Error('JavaScript de Bootstrap requiere JQuery')
+}
 
 /* ========================================================================
- * Bootstrap: transition.js v3.2.0
- * http://getbootstrap.com/javascript/#transitions
+ * Bootstrap: transition.js v3.2.0 (http://getbootstrap.com/javascript/#transitions)
  * ========================================================================
- * Copyright 2011-2014 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * Derechos de Autor 2011-2014 Twitter, Inc.
+ * Licencia bajo MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
-
-+function ($) {
++function($){
   'use strict';
 
-  // CSS TRANSITION SUPPORT (Shoutout: http://www.modernizr.com/)
-  // ============================================================
-
-  function transitionEnd() {
+  // Soporte a Transiciones en CSS (http://www.modernizr.com/)
+  function transitionEnd(){
     var el = document.createElement('bootstrap')
-
+    
     var transEndEventNames = {
       WebkitTransition : 'webkitTransitionEnd',
-      MozTransition    : 'transitionend',
-      OTransition      : 'oTransitionEnd otransitionend',
-      transition       : 'transitionend'
+      MozTransition : 'transitionend',
+      OTransition : 'oTransitionEnd otransitionend',
+      transition : 'transitionend'
     }
 
-    for (var name in transEndEventNames) {
-      if (el.style[name] !== undefined) {
+    for(var name in transEndEventNames){
+      if(el.style[name] !== undefined){
         return { end: transEndEventNames[name] }
       }
     }
-
-    return false // explicit for ie8 (  ._.)
+    return false
   }
 
-  // http://blog.alexmaccaw.com/css-transitions
-  $.fn.emulateTransitionEnd = function (duration) {
+  //http://blog.alexmaccaw.com/css-transitions
+  $.fn.emulateTransitionEnd = function(duration){
     var called = false
     var $el = this
-    $(this).one('bsTransitionEnd', function () { called = true })
-    var callback = function () { if (!called) $($el).trigger($.support.transition.end) }
+    $(this).one('bsTransitionEnd', function (){ called = true })
+    var callback = function(){ if(!called) $($el).trigger($.support.transition.end) }
     setTimeout(callback, duration)
     return this
   }
 
-  $(function () {
+  $(function(){
     $.support.transition = transitionEnd()
 
-    if (!$.support.transition) return
+    if(!$.support.transition){ return }
 
     $.event.special.bsTransitionEnd = {
       bindType: $.support.transition.end,
       delegateType: $.support.transition.end,
-      handle: function (e) {
-        if ($(e.target).is(this)) return e.handleObj.handler.apply(this, arguments)
+      handle: function(e){
+        if($(e.target).is(this)){
+          return e.handleObj.handler.apply(this, arguments)
+        }
       }
     }
   })
--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -2,43 +2,41 @@
  * cbpAnimatedHeader.js v1.0.0
  * http://www.codrops.com
  *
- * Licensed under the MIT license.
+ * Licencia bajo MIT.
  * http://www.opensource.org/licenses/mit-license.php
  * 
- * Copyright 2013, Codrops
+ * Derechos de Autor 2013, Codrops
  * http://www.codrops.com
- */
-var cbpAnimatedHeader = (function() {
+*/
 
+var cbpAnimatedHeader = (function(){
 	var docElem = document.documentElement,
-		header = document.querySelector( '.navbar-fixed-top' ),
+		header = document.querySelector('.navbar-fixed-top'),
 		didScroll = false,
 		changeHeaderOn = 300;
 
-	function init() {
-		window.addEventListener( 'scroll', function( event ) {
-			if( !didScroll ) {
+	function init(){
+		window.addEventListener('scroll', function(event){
+			if(!didScroll){
 				didScroll = true;
-				setTimeout( scrollPage, 250 );
+				setTimeout(scrollPage, 250);
 			}
-		}, false );
+		}, false);
 	}
 
-	function scrollPage() {
+	function scrollPage(){
 		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
-		}
-		else {
-			classie.remove( header, 'navbar-shrink' );
+		if(sy >= changeHeaderOn){
+			classie.add(header, 'navbar-shrink');
+		}else{
+			classie.remove(header, 'navbar-shrink');
 		}
 		didScroll = false;
 	}
 
-	function scrollY() {
+	function scrollY(){
 		return window.pageYOffset || docElem.scrollTop;
 	}
-
+	
 	init();
-
 })();
